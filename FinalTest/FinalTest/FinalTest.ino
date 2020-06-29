280e79ba6706805c4d897019e41151be0e266b5c
tomorrow9913 <tomorrow9913@gmail.com>
2020-06-29 17:21:46 +0900

    final 보고서 제출
    .

--- FinalTest/FinalTest/FinalTest.ino
+++ FinalTest/FinalTest/FinalTest.ino
@@ -1,46 +1,144 @@
-/*
- Name:		IRremote.ino
- Created:	6/4/2020 11:31:41 AM
- Author:	tomor
-*/
+/****************************************************************
+ * File Name:   Final.ino
+ *
+ * Description: 마지막 공학 설계 RC제어 코드
+ * Created:	6/18/2020 11:31:41 AM
+ * Author:	Jeong MinGyu
+ *
+ * Extern File
+ * Name                             Description
+ * ================================ ============================
+ * boarddefs.h                      IR 센서 사용
+ * ir_Lego_PF_BitStreamEncoder.h    IR 센서 사용
+ * IRremoteInt.h                    IR 센서 사용
+ * IRremote.h                       IR 센서 사용
+ * Servo.h                          서보 모터(바퀴) 구동
+ *
+ * Extern Variables
+ * Name         Type        Description
+ * ============ =========== ====================================
+ * None
+ *
+ * Global Variables
+ * Name         Type        Description
+ * ============ =========== ====================================
+ * Dir          enum        주행 방향
+ * leftStandard int         좌측 바퀴 정지 위치
+ * rightStandard int        우측 바퀴 정지 위치
+ * mode         int         주행 모드(1-조작주행 0-자율주행)
+ ****************************************************************/
 
-#include <ir_Lego_PF_BitStreamEncoder.h>
-#include <IRremoteInt.h>
-#include <IRremote.h>
-#include <boarddefs.h>
-#include <Servo.h>
+#include <ir_Lego_PF_BitStreamEncoder.h>        
+#include <IRremoteInt.h>                        
+#include <IRremote.h>                           
+#include <boarddefs.h>                          
 
-int MOTORL_PIN = 5;
-int MOTORR_PIN = 9;
-int TRIG_PIN = 3;
-int ECHO_PIN = 4;
+#include <Servo.h>                              
 
+// 모터구동 핀
+#define MOTORL_PIN 5
+#define MOTORR_PIN 9
+
+// 초음파 센서
+#define TRIG_PIN 3
+#define ECHO_PIN 4
+
+// QTI센서
 #define qtiL 11
 #define qtiR 12
 
+// 리모컨 제어
+#define FRONT 0xFF02FD
+#define BACK 0xFF9867
+#define RIGHT 0xFF906F
+#define LEFT 0xFFE01F
+#define STOP 0xFFA857
+#define MODE 0xFF22DD
+
+// 서보 모터
 Servo LServo;
 Servo RServo;
 
+// IR센서
 IRrecv irrecv(2);
 decode_results results;
 
-// the setup function runs once when you press reset or power the board
+// 방향
+enum Dir
+{
+    forward = 0,
+    backward = 1,
+    left = 2,
+    right = 3
+};
+
+int leftStandard = 90;      // 좌측 바퀴 정지 값
+int rightStandard = 90;     // 우측 바퀴 정지 값
+int mode = 1;               // 조작 모드 (1-조작주행 0-자율주행)
+
+/****************************************************************
+* Function Name: setup
+* Description:     
+* the setup function runs once when you press reset or power the board
+*
+* @return: void
+* @param: void
+* Name         Type        Description
+* ============ =========== ====================================
+*
+* Author:       Jeong MinGyu
+* Creat Date:   2020.06.18.
+* Last Edit:    2020.06.18.
+*
+* Global Variables
+* Name         Type        Description
+* ============ =========== ====================================
+* None
+*
+* Algorithm :
+***************************************************************/
 void setup() {
     Serial.begin(9600);
+
+    // QTI 센서
     pinMode(qtiL, INPUT);
     pinMode(qtiR, INPUT);
 
+    // IR 센서
     Serial.println("Enabling IRin");
     irrecv.enableIRIn();
     Serial.println("Enabled IRin");
 
+    // 초음파 센서
     pinMode(TRIG_PIN, OUTPUT);
     pinMode(ECHO_PIN, INPUT);
 
+    // 모터 설정
     LServo.attach(MOTORL_PIN);
     RServo.attach(MOTORR_PIN);
 }
 
+/****************************************************************
+* Function Name: SonarDistance
+* Description:
+* 벽과의 거리를 초음파 센서를 통해 측정한다.
+*
+* @return:  int (벽과의 거리)
+* @param:   void
+* Name         Type        Description
+* ============ =========== ====================================
+*
+* Author:       Jeong MinGyu
+* Creat Date:   2020.06.18.
+* Last Edit:    2020.06.18.
+*
+* Global Variables
+* Name         Type        Description
+* ============ =========== ====================================
+* None
+*
+* Algorithm :
+***************************************************************/
 int SonarDistance() {
     int sonarPwn;
 
@@ -52,49 +150,92 @@ int SonarDistance() {
     digitalWrite(TRIG_PIN, LOW);
 
     sonarPwn = pulseIn(ECHO_PIN, 1, 18500L);
+    
     return sonarPwn / 60;
 }
 
-int leftStandard = 90;
-int rightStandard = 90;
+/****************************************************************
+* Function Name: Move
+* Description:
+* 자체가 이동하도록 바퀴를 동작합니다.
+*
+* @return:  void
+* @param:   int
+* Name         Type        Description
+* ============ =========== ====================================
+* speed        int          값이 클수록 이동(전환) 속도 빠름
+* mode         Dir          이동 방향을 지정합니다
+*
+* Author:       Jeong MinGyu
+* Creat Date:   2020.06.18.
+* Last Edit:    2020.06.18.
+*
+* Global Variables
+* Name         Type        Description
+* ============ =========== ====================================
+* leftStandard  int         좌측 바퀴가 멈추는 값
+* rightStandard int         우측 바퀴가 정지하는 값
+*
+* Algorithm :
+* num   dir	좌	우  %3   %2
+* 0	    전	+	-   0    0
+* 1     후	-	+   1    1
+* 2	    좌	-	-   2    0
+* 3	    우	+	+   0    1
+***************************************************************/
+void Move(int speed, Dir mode) {
+    int LSpeed = mode % 3 ? -speed : speed;
+    int RSpeed = mode % 2 ? speed : -speed;
 
-void TurnLeft(int speed) {
-    LServo.write(leftStandard - speed); // +?? ?? -?? ??
-    RServo.write(rightStandard - speed); // -?? ?? +?? ??
+    LServo.write(leftStandard - speed);     // +is Front -is Back
+    RServo.write(rightStandard - speed);    // -is Front  +is Back
 }
 
-void TurnRight(int speed) {
-    LServo.write(leftStandard + speed); // +?? ?? -?? ??
-    RServo.write(rightStandard + speed); // -?? ?? +?? ??
-}
-
-void goForward(int speed) {
-    LServo.write(leftStandard + speed); // +?? ?? -?? ??
-    RServo.write(rightStandard - speed); // -?? ?? +?? ??
-}
-
-// the loop function runs over and over again until power down or reset
-int mode = 1;
+/****************************************************************
+* Function Name: loop
+* Description:
+* the loop function runs over and over again until power down or reset
+*
+* @return:  void
+* @param:   void
+* Name         Type        Description
+* ============ =========== ====================================
+*
+* Author:       Jeong MinGyu
+* Creat Date:   2020.06.18.
+* Last Edit:    2020.06.18.
+*
+* Global Variables
+* Name         Type        Description
+* ============ =========== ====================================
+* mode         int         주행 모드(1-조작주행 0-자율주행): 1이 기본값
+*
+* Algorithm :
+***************************************************************/
 void loop() {
+    // 조작시 기본 속도
     int speed = 15;
 
+    // 블루투스 조작
     if (Serial.available()) {
         char value = Serial.read();
         Serial.println(value);
 
-
         switch (value)
         {
+        //회전시 조작이 편하도록 속도 를 절반으로
         case '1':
-            TurnLeft(0.5 * speed); break;
+            Move(0.5 * speed, left); break;
         case '2':
-            goForward(speed); break;
+            Move(speed, forward); break;
         case '3':
-            TurnRight(0.5 * speed); break;
+            Move(0.5 * speed, right); break;
         case '4':
-            goForward(-speed); break;
+            Move(speed, backward); break;
         case '5':
-            goForward(0); break;
+            Move(0, forward); break;
+        
+        // 모드 변경 토글
         case '6':
             mode = mode ? 0 : 1; break;
         default:
@@ -102,17 +243,19 @@ void loop() {
         }
     }
 
+    // IR 센서 제어
     if (irrecv.decode(&results)) {
         int count = results.rawlen;
 
         switch (results.value)
         {
-        case 0xFF02FD: goForward(speed); delay(30); break;
-        case 0xFF9867: goForward(-(speed)); delay(30); break;
-        case 0xFF906F: TurnRight(0.5 * speed); delay(30); break;
-        case 0xFFE01F: TurnLeft(0.5 * speed); delay(30); break; 
-        case 0xFFA857: goForward(0); delay(30); break;
-        case 0xFF22DD: mode = mode==1?0:1;
+        case FRONT: Move(speed, forward); delay(30); break;
+        case BACK: Move(speed, backward); delay(30); break;
+        case RIGHT: Move(0.5 * speed, right); delay(30); break;
+        case LEFT: Move(0.5 * speed, left); delay(30); break; 
+        case STOP: Move(0, forward); delay(30); break;
+        // 모드 변경 토글
+        case MODE: mode = mode ? 0 : 1; break;
                 
         default:
             break;
@@ -121,31 +264,34 @@ void loop() {
         irrecv.resume();
     }
 
-    if (mode==0) {
+    // QTI센서를 이용한 자율 주행
+    if (!mode) {
         int LeftQti = digitalRead(qtiL);    // black 1 else 0
         int RightQti = digitalRead(qtiR);
-
-        int distance = SonarDistance();
-
+        
         Serial.println(LeftQti);
         Serial.println(RightQti);
 
-        int standard = 90;
+        // 벽 피하는 기능 추가시 사용 속도를 위해 비활성화
+        // int distance = SonarDistance();
 
-        Serial.print("distance : ");
-        Serial.println(distance);
+        // Serial.print("distance : ");
+        // Serial.println(distance);
 
+        // 좌측이 검은색이면 좌측 방향으로 이동
         if (LeftQti) {
-            TurnLeft(speed-5);
+            Move(speed-5, left);
             delay(3);
         }
+        // 우측이 검은색이면 우측방향으로 이동
         else if (RightQti) {
-            TurnRight(speed-5);
+            Move(speed-5, right);
             delay(3);
         }
+        // 이외는 전진 주행
         else
         {
-            goForward(speed);
+            Move(speed, forward);
         }
     }
 }
